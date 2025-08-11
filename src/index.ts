1255d6077df7e7eb422bbfa7f4adfd46f533e007
Vladislav Kuznetsov <vl.kuz.ru@gmail.com>
2025-08-11 11:47:40 +0700

    resetting computer

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export type { BotProps } from './components/Bot';
+export type { BubbleProps } from './features/bubble/components/Bubble';
+export type { WebSocketMessage, WebSocketConfig } from './utils/websocket';
